422218edb126c0a43f6e1f9f2810195196ceee2a
Tomoyuki Maehara <marvin@Tomoyukis-MacBook-Pro.local>
2015-04-09 13:32:05 +0900

    Initial Commit

---
+++ OpenCLTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenCLTest
+//
+//  Created by Tomoyuki Maehara on 4/9/15.
+//  Copyright (c) 2015 Tomoyuki Maehara. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
